448fedcf3edc967d50b0a829909bd983c666717d
Fiona <fionagacquin@live.co.uk>
2023-01-05 15:53:33 +0000

    Merging score_board

--- code/score_board.py
+++ code/score_board.py
@@ -1,10 +1,8 @@
 from PyQt6.QtGui import QIcon, QPixmap, QFont
 from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel, \
-    QPushButton  # TODO import additional Widget classes as desired
-from PyQt6.QtCore import pyqtSlot, Qt, QTimer
+    QPushButton, QDialog, QGridLayout
+from PyQt6.QtCore import pyqtSlot, Qt
 
-from board import Board
-from game_logic import GameLogic
 
 
 class ScoreBoard(QDockWidget):
@@ -12,39 +10,57 @@ class ScoreBoard(QDockWidget):
 
     def __init__(self):
         super().__init__()
+        self.player2 = None
+        self.player1 = None
         self.initUI()
 
     def initUI(self):
         '''initiates ScoreBoard UI'''
         self.resize(250, 250)
-        self.setFixedWidth(200)
+        self.setFixedWidth(250)
         self.setAllowedAreas(Qt.DockWidgetArea.RightDockWidgetArea)
         self.center()
         self.setWindowTitle('ScoreBoard')
-
-        #create a widget to hold other widgets
-        self.mainWidget = QWidget()  #main scoreboard widgets
-        self.mainLayout = QVBoxLayout()   #layout style initialized
-        self.mainWidget.setMaximumSize(200, 800)  # setting max size for side widget
+        self.player1 = ""
+        self.player2 = ""
+        # create a widget to hold other widgets
+        self.mainWidget = QWidget()  # main scoreboard widgets
+        self.mainLayout = QVBoxLayout()  # layout style initialized
+        self.mainWidget.setMaximumSize(250, 1000)  # setting max size for side widget
         self.setWidget(self.mainWidget)
         self.show()
 
         # styling and background for dock
         self.mainWidget.setAutoFillBackground(True)
-        self.mainWidget.setStyleSheet("""background-image: url("icons/p6.png");""")
+        self.mainWidget.setStyleSheet("""background-image: url("icons/beige-tiles.png");""")
 
         # used for font across widget
         self.timer_font = QFont('Baskerville', 16)  # font for timer
         self.timer_font.setBold(True)  # timer bold
-
+        self.current_font = QFont('Baskerville', 18)  # font for timer
         # create two labels which will be updated by signals
-        self.label_clickLocation = QLabel("Click Location: ")
-
+        self.scoreBlack = 0
+        self.scoreWhite = 0
+        # label for territory occupied
+        self.territory = QLabel("Territory : " + "Territory : "  + "\n " )
+        self.territory.setStyleSheet("color:#003049; font-weight: bold")
+        self.territory.setFont(QFont('Baskerville', 16))
+        # self.label_clickLocation = QLabel()
+        # label for stones collected by players
+        self.label_collected = QLabel(
+            "    Black : " + str(self.scoreBlack) + "    White : " + str(self.scoreWhite) + "\n ")
+        self.label_collected.setFont(QFont('Baskerville', 16))
+        self.label_collected.setStyleSheet("font-weight: bold;  color:#003049;")
         # current turn labels
         self.turn_label = QLabel("Current Turn:")
-        self.turn_label.setFont(self.timer_font)
-        self.curent_player = QLabel("") # ****player 1 label empty string call flag
-
+        self.turn_label.setFont(self.current_font)
+        self.turn_label.setStyleSheet("color:#2b9348; font-weight: bold")
+        # display player's name by turn
+        self.curent_player = QLabel("")  #player 1 label empty string call flag color
+        self.curent_player.setFont(QFont('Baskerville', 16))
+        # user turn by color, adding image white or black to label
+        self.curent_turn = QLabel()
+        self.curent_turn.setStyleSheet("""background-image: url("icons/black.png");""")
 
         #  Clock icons and labels
         self.stop_watch = QPixmap('./icons/stopwatch.png')  # icons for timer blue
@@ -52,39 +68,35 @@ class ScoreBoard(QDockWidget):
         self.stop_watch_label = QLabel()
         self.stop_watch_label.setPixmap(self.stop_watch)  # adding icon for timer
         self.stop_watch_label.setAlignment(Qt.AlignmentFlag.AlignCenter)  # icon alignment
-
-
-        self.label_timeRemaining = QLabel("Time: ") # value label
-        self.label_timeRemaining.setFont(self.timer_font) # set font
-        self.label_timeRemaining.setAlignment(Qt.AlignmentFlag.AlignCenter) # allin value
-
-
-
-
-        #  EXTRA FEATURES
+        # label display value left of seconds
+        self.label_timeRemaining = QLabel("Time: ")  # value label
+        self.label_timeRemaining.setFont(self.timer_font)  # set font
+        self.label_timeRemaining.setAlignment(Qt.AlignmentFlag.AlignCenter)  # align value
 
         """add skip button"""
         self.skip_button = QPushButton(QIcon("./icons/skip.png"), "Skip", self)
-        # self.skip_button.clicked.connect(GameLogic.setPlayerPassed(Board.logic.checkTurn(), "true"))  # calls skip turn method upon clicking
+        # self.skip_button.clicked.connect(self.skipTurn)  # calls skip turn method upon clicking
+        # Will leave skip turn method being called from the go file for now
 
-        """"add end button"""
+        """"add STOP button;ignore play"""
         self.play_button = QPushButton(QIcon("./icons/close.png"), "Stop")
-        self.play_button.setShortcut('Ctrl+n')
+        self.play_button.setShortcut('Ctrl+s')
         self.play_button.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-        # !!self.play_button.clicked.connect(self....)  # upon clicking triggers the game stop
-
 
         # adding labels to right dock widget
         self.mainWidget.setLayout(self.mainLayout)  # layout of dock
-        self.mainLayout.addWidget(self.label_clickLocation)  # location label to widget
+        self.mainLayout.addWidget(self.label_collected)  # location label to widget
+        self.mainLayout.addWidget(self.territory) # label for displaying territory
+        #self.mainLayout.addWidget(self.label_clickLocation)  # location label to widget
         self.mainLayout.addWidget(self.stop_watch_label)  # add icon label  to widget
         self.mainLayout.addWidget(self.label_timeRemaining)  # time value label to widget
-        self.mainLayout.addWidget(self.turn_label)
-        self.mainLayout.addWidget(self.curent_player)
-        self.mainLayout.addWidget(self.skip_button)
-        self.mainLayout.addWidget(self.play_button)
+        self.mainLayout.addWidget(self.turn_label) #turn text
+        self.mainLayout.addWidget(self.curent_player) #players turn
+        self.mainLayout.addWidget(self.curent_turn) #color turn
+        self.mainLayout.addWidget(self.skip_button) #skip button
+        self.mainLayout.addWidget(self.play_button) #stop
 
-        # styling for play button
+        # styling for stop button
         self.play_button.setStyleSheet(""" 
                       QPushButton {
                           font-weight:1000;
@@ -114,24 +126,29 @@ class ScoreBoard(QDockWidget):
                       }
                   """)
 
+    """function for Play button"""
+    def getPlayButton(self):
+        return self.play_button
 
     def center(self):
         '''centers the window on the screen, you do not need to implement this method'''
 
     def make_connection(self, board):
         '''this handles a signal sent from the board class'''
+        self.gameBoard = board
         # when the clickLocationSignal is emitted in board the setClickLocation slot receives it
         board.clickLocationSignal.connect(self.setClickLocation)
         # when the updateTimerSignal is emitted in the board the setTimeRemaining slot receives it
         board.updateTimerSignal.connect(self.setTimeRemaining)
 
-    """EXTRA FEATURE updating the timer counter"""
-    @pyqtSlot(str) # checks to make sure that the following slot is receiving an argument of the type 'int'
+
+    @pyqtSlot(str)  # checks to make sure that the following slot is receiving an argument of the type 'int'
     def setClickLocation(self, clickLoc):
         '''updates the label to show the click location'''
-        self.label_clickLocation.setText("Click Location:" + clickLoc)
+        #self.label_clickLocation.setText(clickLoc)
         # print('slot ' + clickLoc)
 
+    """EXTRA FEATURE updating the timer counter"""
     @pyqtSlot(int)
     def setTimeRemaining(self, timeRemainng):
         '''updates the time remaining label to show the time remaining'''
@@ -139,11 +156,16 @@ class ScoreBoard(QDockWidget):
         self.label_timeRemaining.setText(update)  # update time
         # print('slot ' + update)
 
-        if timeRemainng < 10: # if 10 seconds left update icon
-            self.stop_watch_label.setPixmap(self.stop_watch_red) #updating icon
-            self.stop_watch_label.update() #icon update
+        if timeRemainng < 10:  # if 10 seconds left update icon
+            self.stop_watch_label.setPixmap(self.stop_watch_red)  # updating icon
+            self.stop_watch_label.update()  # icon update
 
-        #  if timeRemainng<=0:
-        # timeRemainng.stop()
-        #self.timer_over()
-        # self.redraw()
+    """function updated the score of both players"""
+    def updateScores(self, s1, s2):
+        # assign variables
+        self.scoreBlack = s1
+        self.scoreWhite = s2
+        # displaying the stones captured
+        self.label_collected.setText(
+            "\tCaptured Stones\n\nBlack : " + str(self.scoreBlack) + "    White : " + str(self.scoreWhite) + "\n ")
+        self.label_collected.setStyleSheet("font-weight: bold;  color:#003049;")
